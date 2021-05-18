6317135df6dd29f89ede0d0affa27ba43855e9f2
JVSATSU <xhoreka@gmail.com>
2021-05-18 18:42:52 +0100

    Post Pesquisa Destino Completo

--- src/screens/pesquisadestino/index.js
+++ src/screens/pesquisadestino/index.js
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
-import { View, TextInput, Text } from "react-native";
+import { View, TextInput, Text, FlatList } from "react-native";
 import styles from './styles';
+import procuraResultados from '../../../assets/data/procura';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const PesquisaDestinoTela = (props) => {
 
   const [inputText,setInputText] = useState();
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         style={styles.textInput}
         placeholder="Para onde voce vai?"
+        value={inputText}
+        onChangeText={setInputText}
+      />
+
+
+        <FlatList 
+        data={procuraResultados}
+        renderItem= {({item})=> (
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Entypo name={"location-pin"} size={35} />
+            </View>
+          <Text style={styles.destinotext}>{item.descricao}</Text>
+        </View>
+        )} 
       />
     </View>
   );
--- src/screens/pesquisadestino/styles.js
+++ src/screens/pesquisadestino/styles.js
@@ -6,7 +6,22 @@ const styles = StyleSheet.create({
   },
   textInput: {
     fontSize: 20,
+    marginBottom: 20,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  iconContainer: {
+    backgroundColor: '#e7e7e7',
+    padding: 7,
+    borderRadius: 10,
+    marginLeft: 15,
+  },
+  destinotext: {},
 });
 
 export default styles;
