de25abc2586b09656763c304210a23b307102d81
JVSATSU <xhoreka@gmail.com>
2021-05-18 14:50:20 +0100

    Data do Procura

---
+++ assets/data/procura.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: '0',
+    descricao: 'Tenerife, Canary Islands',
+  },
+  {
+    id: '1',
+    descricao: 'Tenerife, Santa Cruz de Tenerife',
+  },
+  {
+    id: '2',
+    descricao: 'Tenerife, El Medano',
+  },
+  {
+    id: '3',
+    descricao: 'Tenerife, Costa Adeje',
+  },
+  {
+    id: '4',
+    descricao: 'Tenerife, Puerto de la Cruz',
+  },
+];
--- src/screens/pesquisadestino/index.js
+++ src/screens/pesquisadestino/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Text } from "react-native";
 import styles from './styles';
 
 const PesquisaDestinoTela = (props) => {
+
+  const [inputText,setInputText] = useState();
+
   return (
     <View>
-      <Text>React é Top!</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Para onde voce vai?"
+      />
     </View>
   );
 };
