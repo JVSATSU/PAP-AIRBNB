05bd7480b8cfeafc5853e9bb6b04733b710121e1
JVSATSU <xhoreka@gmail.com>
2021-06-01 14:53:26 +0100

    Update HomeTabNavigator.js

--- src/navigation/HomeTabNavigator.js
+++ src/navigation/HomeTabNavigator.js
@@ -1,11 +1,29 @@
 import React from "react";
 import {View, Text} from "react-native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/home';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+import 
+
+
+
+const Tab = createBottomTabNavigator();
 
 const HomeTabNavigator = (props) => {
     return (
-        <View>
-            <Text>React é MUITO FODA!!!</Text>
-        </View>
+        <Tab.Navigator>
+          <Tab.Screen
+            name={"Explore"}
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color:}) => (
+
+              )
+            }}
+          />
+        </Tab.Navigator>
     );
 };
 
