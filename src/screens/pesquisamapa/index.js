1663178ece0159ea1da410486aa1deda146a37e8
JVSATSU <xhoreka@gmail.com>
2021-07-07 03:26:31 +0100

    Carrossel sincronizado com Mapa

--- src/screens/pesquisamapa/index.js
+++ src/screens/pesquisamapa/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {View, Text} from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import lugares from '../../../assets/data/feed';
@@ -10,11 +10,39 @@ import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimen
 const Index = () => {
   const [LugarSelecionadoId, setLugarSelecionadoId] = useState(null);
 
+  const flatlist = useRef();
+  const mapa = useRef();
+
   const width = useWindowDimensions().width;
+
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  const onViewChanged = useRef(({viewableItems}) =>{
+    if(viewableItems.length > 0){
+      const LugarSelecionado = viewableItems[0].item;
+      setLugarSelecionadoId(LugarSelecionado.id)
+    }})
+
+  useEffect(() => {
+      if (!LugarSelecionadoId || !flatlist){
+          return;
+      }
+      const index = lugares.findIndex(place => place.id === LugarSelecionadoId)
+      flatlist.current.scrollToIndex({index})
+
+      const LugarSelecionado = lugares[index];
+      const regiao = {
+          latitude: LugarSelecionado.coordinate.latitude,
+          longitude: LugarSelecionado.coordinate.longitude,
+          latitudeDelta: 0.8,
+          longitudeDelta: 0.8,
+      }
+        mapa.current.animateToRegion(regiao);
+  }, [LugarSelecionadoId])
 
   return (
     <View>
       <MapView
+          ref={mapa}
         style={{width: '100%', height: '100%'}}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -34,6 +62,7 @@ const Index = () => {
       </MapView>
       <View style={{position: 'absolute', bottom: 20}}>
         <FlatList
+            ref={flatlist}
           data={lugares}
           renderItem={({item}) => <PostCarrossel post={item} />}
           horizontal
@@ -41,6 +70,9 @@ const Index = () => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
+
         />
       </View>
     </View>
